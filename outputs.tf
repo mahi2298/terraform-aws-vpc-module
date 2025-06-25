d30635a160c1db14170e2c2d799c701f56635359
maheswari <mahi.sgs22@gmail.com>
2025-06-25 13:40:42 +0530

    vpc module

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
 output "vpc_id" {
     value = aws_vpc.main.id # getting the vpcid from vpc.tf
 }
+
+output "public_subnet_id" {
+    value = aws_subnet.public[*].id # getting the vpcid from vpc.tf
+}
+
+output "private_subnet_id" {
+    value = aws_subnet.private[*].id # getting the vpcid from vpc.tf
+}
